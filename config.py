e813d1aea7af58b94f0d00eabf2e6e5e400b280d
Artur Michalek <artumi1@gmail.com>
2021-05-05 17:05:32 +0200

    Add actual config

--- config.py
+++ config.py
@@ -1,2 +1,6 @@
+import os
+
 class Config(object):
-    pass
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or 'sqlite:///'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
