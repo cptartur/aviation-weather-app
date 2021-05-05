6d6acb0e5b4b018408050303121a55c4ae877717
Artur Michalek <artumi1@gmail.com>
2021-05-05 20:29:50 +0200

    Register auth blueprint

--- avw/__init__.py
+++ avw/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from config import Config
 from avw.main import bp as main_bp
 
-db = SQLAlchemy()
 
+db = SQLAlchemy()
+login_manager = LoginManager()
 
 def create_app(config=Config):
     app = Flask(__name__)
@@ -15,9 +17,13 @@ def create_app(config=Config):
     #     pass
 
     db.init_app(app)
+    login_manager.init_app(app)
 
     app.register_blueprint(main_bp)
 
+    from avw.auth import bp as auth_bp
+    app.register_blueprint(auth_bp)
+    
     return app
 
 from avw import models
