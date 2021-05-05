1c7785aaeea21f70cd4d06a6638116a4bf438a39
Artur Michalek <artumi1@gmail.com>
2021-05-05 20:28:02 +0200

    Add id field to User model

--- avw/models.py
+++ avw/models.py
@@ -1,8 +1,9 @@
 from avw import db
 
 
-class User(db.Model):
-    username = db.Column(db.String(64), primary_key=True)
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64))
     email = db.Column(db.String(120), unique=True)
     password_hash = db.Column(db.String(128))
     preferences = db.Column(db.JSON())
