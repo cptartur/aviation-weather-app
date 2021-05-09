dcdf68fa560bc9c1c70a91e7e3902a1d0e52ddca
Artur Michalek <artumi1@gmail.com>
2021-05-09 23:56:16 +0200

    Add login, password reset tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,5 +1,8 @@
 from avw import db
 from avw.models import User, load_user
+from flask import Response, current_app
+from jwt import encode
+from time import time
 
 
 def test_load_user(client, new_user: User):
@@ -13,3 +16,46 @@ def test_load_user(client, new_user: User):
     assert load_user('fdsafskaj') is None
     assert load_user('') is None
     assert load_user('2\3sd') is None
+
+
+def test_login_user(client):
+    user = User('Susan', 'a@a.com', '12345')
+    db.session.add(user)
+    db.session.commit()
+
+    rv: Response = client.post(
+        '/login', data={'username': 'Susan', 'password': '12345'},
+        follow_redirects=True)
+    assert rv.status_code == 200
+    assert b'Succesfully logged in user' in rv.data
+
+    db.session.delete(user)
+    db.session.commit()
+
+def test_password_reset(client):
+    user = User('Susan', 'a@a.com', '12345')
+    db.session.add(user)
+    db.session.commit()
+
+    t = user.get_reset_password_token()
+    assert t is not None
+    assert type(t) is str
+
+    r_user = User.verify_reset_password_token(t)
+    assert r_user == user
+
+    db.session.delete(user)
+    db.session.commit()
+
+
+def test_password_reset_exp_time(client):
+    user = User('Susan', 'a@a.com', '12345')
+    db.session.add(user)
+    db.session.commit()
+
+    t1 = encode({'user_id': user.id, 'exp': time() - 10000},
+                current_app.config.get('SECRET_KEY'), algorithm='HS256')
+    t2 = user.get_reset_password_token()
+    assert User.verify_reset_password_token(t1) is None
+    assert User.verify_reset_password_token(t2) is not None
+    assert User.verify_reset_password_token(t2) == user
