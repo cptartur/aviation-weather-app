9e53d8c9164ef5cf86052bf38a0e1c5cb5bcd589
Artur Michałek <52135326+cptartur@users.noreply.github.com>
2021-05-17 21:07:44 +0200

    Add airport search functionality (#2)
    * Add elasticsearch
    
    * Update search conditions
    
    * Add caching of recent searches

--- avw/main/routes.py
+++ avw/main/routes.py
@@ -1,20 +1,48 @@
 from flask.globals import request
 from flask.helpers import url_for
 from flask_wtf import form
+from avw.search import search_airport
 from avw.main.forms import SearchForm
-from flask import render_template, redirect
 from avw.main import bp
+from flask import render_template, redirect, session
+from flask_login import current_user
 from metar.metar import Metar
 
 m = Metar()
+
 
 @bp.route('/', methods=('GET', 'POST'))
 @bp.route('/index', methods=('GET', 'POST'))
 def index():
     search_form = SearchForm()
-    if request.method == 'POST' and search_form.validate_on_submit():
-        return redirect(url_for('main.airport', code=search_form.airport_name.data))
-    return render_template('index.html', form=search_form)
+    favorite_apts = None
+    if current_user.is_authenticated:
+        favorite_apts = current_user.get_favorite_airports()
+    if search_form.validate_on_submit():
+        return redirect(
+            url_for('main.search', code=search_form.airport_name.data))
+    return render_template('index.html', form=search_form,
+                           recent_apts=session.get('recent_apts'),
+                           favorite_apts=favorite_apts)
+
+
+@bp.route('/search/<code>', methods=['GET'])
+def search(code):
+    results = search_airport(code)
+    if results is not None:
+        if 'recent_apts' not in session:
+            session['recent_apts'] = [code]
+        else:
+            if code not in session['recent_apts']:
+                if len(session['recent_apts']) >= 5:
+                    session['recent_apts'] = session['recent_apts'][0:4]
+                session['recent_apts'].insert(0, code)
+                session.modified = True
+        return render_template('search.html',
+                               title=f'Search results for {code}',
+                               results=results)
+    return redirect(url_for('main.index'))
+
 
 @bp.route('/airport/<code>')
 def airport(code):
---
+++ avw/search.py
@@ -0,0 +1,38 @@
+from flask import current_app
+from collections import OrderedDict
+from elasticsearch import TransportError
+
+
+def search_airport(name) -> list:
+    if getattr(current_app, 'elasticsearch', None) is None:
+        return None
+    try:
+        q: dict = current_app.elasticsearch.search(
+            index=current_app.config['SEARCH_INDEX_NAME'],
+            body={
+                'query': {
+                    'bool': {
+                        'must': [
+                            {
+                                'multi_match': {
+                                    'query': name,
+                                    'type': 'phrase_prefix',
+                                    'fields': ['ident', 'name', 'municipality'],
+                                }
+                            }
+                        ],
+                        'filter': [
+                            {
+                                'terms': {'type': ['large_airport', 'medium_airport']}
+                            }
+                        ]
+                    }
+                }
+            }
+        )
+    except TransportError as e:
+        return None
+    if q['timed_out'] is False and q['hits']['total']['value'] > 0:
+        return sorted(q['hits']['hits'], key=lambda k: k['_score'], reverse=True)
+    else:
+        return None
