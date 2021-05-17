d6a18c8d83709d9a57cfddea481da1af0f56ab8f
Artur Michalek <artumi1@gmail.com>
2021-05-17 18:42:02 +0200

    Add caching of recent searches

--- avw/main/routes.py
+++ avw/main/routes.py
@@ -4,24 +4,40 @@ from flask_wtf import form
 from avw.search import search_airport
 from avw.main.forms import SearchForm
 from avw.main import bp
-from flask import render_template, redirect
+from flask import render_template, redirect, session
+from flask_login import current_user
 from metar.metar import Metar
 
 m = Metar()
+
 
 @bp.route('/', methods=('GET', 'POST'))
 @bp.route('/index', methods=('GET', 'POST'))
 def index():
     search_form = SearchForm()
+    favorite_apts = None
+    if current_user.is_authenticated:
+        favorite_apts = current_user.get_favorite_airports()
     if search_form.validate_on_submit():
-        return redirect(url_for('main.search', code=search_form.airport_name.data))
-    return render_template('index.html', form=search_form)
+        return redirect(
+            url_for('main.search', code=search_form.airport_name.data))
+    return render_template('index.html', form=search_form,
+                           recent_apts=session.get('recent_apts'),
+                           favorite_apts=favorite_apts)
 
 
 @bp.route('/search/<code>', methods=['GET'])
 def search(code):
     results = search_airport(code)
     if results is not None:
+        if 'recent_apts' not in session:
+            session['recent_apts'] = [code]
+        else:
+            if code not in session['recent_apts']:
+                if len(session['recent_apts']) >= 5:
+                    session['recent_apts'] = session['recent_apts'][0:4]
+                session['recent_apts'].insert(0, code)
+                session.modified = True
         return render_template('search.html',
                                title=f'Search results for {code}',
                                results=results)
--- tests/test_search.py
+++ tests/test_search.py
@@ -1,4 +1,5 @@
 from avw.search import search_airport
+from flask import session
 
 
 def test_search_airport(client):
@@ -29,3 +30,26 @@ def test_search_invalid_input(client):
     assert search_airport('231123') is None
     assert search_airport([]) is None
     assert search_airport({}) is None
+
+
+def test_session(client):
+    client.get('/')
+    assert 'recent_apts' not in session
+
+    client.get('/search/EPKK', follow_redirects=True)
+    assert 'recent_apts' in session
+    assert 'EPKK' in session['recent_apts'] 
+    assert session['recent_apts'][0] == 'EPKK'
+
+    client.get('/search/KSFO')
+    client.get('/search/EPGD')
+    client.get('/search/EPWA')
+    client.get('/search/EDDH')
+    assert {'KSFO', 'EPGD', 'EPWA', 'EDDH'}.issubset(session['recent_apts'])
+    assert 'EPKK' in session['recent_apts'] 
+    
+    client.get('/search/KLAX')
+    assert len(session['recent_apts']) == 5
+    assert 'KLAX' in session['recent_apts'] 
+    assert {'KSFO', 'EPGD', 'EPWA', 'EDDH'}.issubset(session['recent_apts'])
+    assert 'EPKK' not in session['recent_apts']
