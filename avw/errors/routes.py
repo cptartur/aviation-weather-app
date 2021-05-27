85987fcfe84171a4154d70bbad767d1a33f28fbc
Artur Michalek <artumi1@gmail.com>
2021-05-27 17:51:57 +0200

    Add image handlers

--- avw/errors/routes.py
+++ avw/errors/routes.py
@@ -1,17 +1,21 @@
 from avw.errors import bp
-from flask import render_template
+from flask import render_template, current_app
+from os import environ
 
 
 @bp.app_errorhandler(404)
 def not_found_error(error):
-    return render_template('404.html'), 404
+    img = environ.get('ERROR_IMG')
+    return render_template('404.html', img=img), 404
 
 
 @bp.app_errorhandler(500)
 def internal_server_error(error):
-    return render_template('500.html'), 500
+    img = environ.get('ERROR_IMG')
+    return render_template('500.html', img=img), 500
 
 
 @bp.route('/476', methods=('GET',))
 def no_metar_for_this_airport_error():
-    return render_template('476.html'), 476
+    img = environ.get('476_IMG')
+    return render_template('476.html', img=img), 476
